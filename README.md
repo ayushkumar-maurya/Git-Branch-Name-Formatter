aa821457ea632a3c563a0c75db43982e64955697
Ayushkumar Maurya <ayushskmaurya@gmail.com>
2025-12-08 23:12:19 +0530

    Completed formatting

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Git-Branch-Name-Formatter
 Just a very simple utility web page to get the formatted Git branch name.
+
+For example, if the branch name is `Dashboard Bug Fixes @Username #Reference_ID`,  
+the formatted branch name will be generated as `dashboard-bug-fixes-username-reference-id`.
--- scripts.js
+++ scripts.js
@@ -1,19 +1,32 @@
-branchNameElement = document.getElementById('branch-name')
-submitBtnElement = document.getElementById('submit-btn')
-outputContainerElement = document.getElementById('output-container')
-formattedBranchNameElement = document.getElementById('formatted-branch-name')
-successMsgElement = document.getElementById('success-msg')
+const branchNameElement = document.getElementById('branch-name')
+const submitBtnElement = document.getElementById('submit-btn')
+const outputContainerElement = document.getElementById('output-container')
+const formattedBranchNameElement = document.getElementById('formatted-branch-name')
+const successMsgElement = document.getElementById('success-msg')
 
 submitBtnElement.addEventListener('click', e => {
+  let branchName
+  let formattedBranchName
+
   outputContainerElement.style.display = 'none'
+
   branchName = branchNameElement.value
   formattedBranchName = formatBranchName(branchName)
   formattedBranchNameElement.innerText = formattedBranchName
-  successMsgElement.innerText = `Formatted branch name was generated successfully for the following branch\n${formattedBranchName}`
+
+  successMsgElement.innerText = `Formatted branch name was generated successfully for the following branch\n${branchName}`
   outputContainerElement.style.display = 'block'
 })
 
-formatBranchName = branchName => {
-  formattedBranchName = `Formatted Branch Name: ${branchName}`
+const formatBranchName = branchName => {
+  let partialName
+  let formattedBranchName
+  
+  partialName = branchName.replace(/[^A-Za-z0-9]/g, ' ')
+  if(partialName.trim().length === 0)
+    partialName = 'New Branch 1'
+  partialName = partialName.replace(/\s+/g, '-').toLowerCase()
+
+  formattedBranchName = partialName
   return formattedBranchName
 }
